7e5b802bb55d7a775bd2ebb390b246fc6b8b91c2
hashnet <maidul.hasan@gmail.com>
2018-12-28 23:43:37 +1100

    Updated HelloClient.java of project 'hellorestlet'.

--- hellorestlet/src/main/java/com/hashnet/hellorestlet/HelloClient.java
+++ hellorestlet/src/main/java/com/hashnet/hellorestlet/HelloClient.java
@@ -2,12 +2,14 @@ package com.hashnet.hellorestlet;
 
 import java.io.IOException;
 
+import org.restlet.representation.Representation;
 import org.restlet.resource.ClientResource;
 import org.restlet.resource.ResourceException;
 
 public class HelloClient {
 	public static void main(String[] args) throws ResourceException, IOException {
 		ClientResource clientResource = new ClientResource("http://localhost:8080");
-		clientResource.get().write(System.out);
+		Representation clientRepresentation = clientResource.get();
+		clientRepresentation.write(System.out);
 	}
 }
